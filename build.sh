b6e7fe2efe01e44019c7edc4e63ab6766695af2e
Rio Astamal <me@rioastamal.net>
2023-02-28 23:40:28 +0000

    Build script: Using temporary dir/ to build node_modules/

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@
 APP_NAME=serverless-todo-api
 ZIP_FILE_NAME=${APP_NAME}.zip
 FILES_TO_ZIP="src/ node_modules/ lambda.js"
+SOURCE_FILES="src package.json package-lock.json lambda.js"
 
 # Zip file will be uploaded to this bucket
 [ -z "$APP_FUNCTION_BUCKET" ] && {
@@ -15,9 +16,17 @@ FILES_TO_ZIP="src/ node_modules/ lambda.js"
     exit 1
 }
 
+rm -rf .build/tmp/ 2>/dev/null
+
+echo "Downloading app dependencies..."
+mkdir -p .build/tmp
+cp -a $SOURCE_FILES .build/tmp/
+cd .build/tmp && npm install --omit=dev
+
 echo "Creating zip..."
-rm .build/$ZIP_FILE_NAME 2>/dev/null
-zip -q -r .build/$ZIP_FILE_NAME $FILES_TO_ZIP
+zip -q -r $ZIP_FILE_NAME $FILES_TO_ZIP
+
+cd ../..
 
 echo "Uploading zip to S3 Bucket..."
-aws s3 cp .build/$ZIP_FILE_NAME s3://$APP_FUNCTION_BUCKET/$ZIP_FILE_NAME
+aws s3 cp .build/tmp/$ZIP_FILE_NAME s3://$APP_FUNCTION_BUCKET/$ZIP_FILE_NAME
